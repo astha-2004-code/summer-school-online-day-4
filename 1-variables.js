3fdd23e4a3b288477e38dde3e8b33b54e7675261
astha-2004-code <231220017@nitdelhi.ac.in>
2025-07-01 09:14:37 +0530

    done task 1

---
+++ 1-variables.js
@@ -0,0 +1,13 @@
+// Task 1: Variable and Data Types
+
+let studentName = "Astha"; // string
+var age = 19; // number
+const isScholar = true; // boolean
+let course; // undefined
+let score = null; // null
+
+console.log(typeof studentName); // string
+console.log(typeof age); // number
+console.log(typeof isScholar); // boolean
+console.log(typeof course); // undefined
+console.log(typeof score); // object (weird JS thing)
