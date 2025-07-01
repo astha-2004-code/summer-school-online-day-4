6044ea1e6b4a87f0d023bb726d0747ffaa437060
astha-2004-code <231220017@nitdelhi.ac.in>
2025-07-01 10:40:47 +0530

    task 4 loops done

---
+++ 4-loops.js
@@ -0,0 +1,21 @@
+console.log("Task 4");
+// Task 4: Loops
+
+console.log("For loop 1 to 10");
+for (let i = 1; i <= 10; i++) {
+  console.log(i);
+}
+
+console.log("While loop: Even numbers 2 to 20");
+let j = 2;
+while (j <= 20) {
+  console.log(j);
+  j += 2;
+}
+
+console.log("Do while: 10 to 1");
+let k = 10;
+do {
+  console.log(k);
+  k--;
+} while (k >= 1);
