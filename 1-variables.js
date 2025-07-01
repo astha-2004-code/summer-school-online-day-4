f6dad7bd619d9a353e3eb8e68bb93d314623bd50
astha-2004-code <231220017@nitdelhi.ac.in>
2025-07-01 10:39:44 +0530

    updated age variable

--- 1-variables.js
+++ 1-variables.js
@@ -1,13 +1,14 @@
 // Task 1: Variable and Data Types
+console.log("Task 1");
 
 let studentName = "Astha"; // string
-var age = 19; // number
+var yourAge = 19; // number
 const isScholar = true; // boolean
 let course; // undefined
 let score = null; // null
 
 console.log(typeof studentName); // string
-console.log(typeof age); // number
+console.log(typeof yourAge); // number
 console.log(typeof isScholar); // boolean
 console.log(typeof course); // undefined
 console.log(typeof score); // object (weird JS thing)
